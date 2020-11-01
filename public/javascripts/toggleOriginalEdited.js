82bf3a0a3deaab367dfab46224b6a7120d54472f
janko12 <henrich.sury97@gmail.com>
2020-11-01 17:46:46 +0100

    corrected misstyped code, adjusted styles
    _post-form.scss - centered config options
    toggleOriginalEdited.js - fixed code, where condition was looking for wrong data attribute

--- public/javascripts/toggleOriginalEdited.js
+++ public/javascripts/toggleOriginalEdited.js
@@ -1,11 +1,11 @@
 const postsContainer = document.querySelector("#posts-container");
 
 postsContainer.addEventListener("click", event =>{
-    if(!event.target.dataset.functionality === "unveil-post")
+    if(event.target.dataset.functionality !== "post-unveil")
         return;
     
-    const postToShow = document.getElementById(`${event.target.dataset.show}`);
-    const postToHide = document.getElementById(`${event.target.dataset.hide}`);
+    const postToShow = document.getElementById(event.target.dataset.show);
+    const postToHide = document.getElementById(event.target.dataset.hide);
 
     postToShow.classList.remove("display-none");
     postToHide.classList.add("display-none");
