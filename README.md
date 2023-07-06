a597da7aeb690a792bc63783bd6489dc4baf6c51
janko12 <henrich.sury97@gmail.com>
2023-07-06 12:42:23 +0200

    Update demo link path
    heroku dropped free Tier, so the app is now eployed on render.com

--- README.md
+++ README.md
@@ -1,31 +1,35 @@
 # Only members website
 
-###  __Introduction__
-Project assignment comes as always from _The Odin Project_ <a href="https://www.theodinproject.com/courses/nodejs/lessons/members-only#assignment" target="_blank">lesson</a>. I really appreciate this website as it teaches beginner developers to become professionals and helps them to chase their dream. If you pay attention to the odin assignment its a bit different, because i wanted to challenge myself and create a bit more sophisticated app. The app was developed over the course of one week straight, 5-7 hours a day including design prototyping in figma, planning the application behaviour and planning code organization structure (model's structure, router's paths,  etc...)
+###  **Introduction**
 
-## __About the app__
+Project assignment comes as always from _The Odin Project_ <a href="https://www.theodinproject.com/courses/nodejs/lessons/members-only#assignment" target="_blank">lesson</a>. I really appreciate this website as it teaches beginner developers to become professionals and helps them to chase their dream. If you pay attention to the odin assignment its a bit different, because i wanted to challenge myself and create a bit more sophisticated app. The app was developed over the course of one week straight, 5-7 hours a day including design prototyping in figma, planning the application behaviour and planning code organization structure (model's structure, router's paths, etc...)
+
+## **About the app**
+
 Mobile friendly, data-driven, user-based application, where users can register, create posts, become member and become an admin. Posts are displayed on the main page. Users that are not signed in will not be able to create post, become admin or become member. Member users will have the ability to change text color of their posts from default (white) to colors that are offered or have shadow on text with different colors by their pick. In addition they have the ability to see original posts of all users. Every user that is signed in will be able to edit his post or delete it. Only admin user can delete whichever post he wants.
 
-
 ### _Frameworks_
+
 1. back-end:
-   - Express
+    - Express
 2. front-end:
-   - \-
+    - \-
 
 ### Takeaways from this project
-a lot of small things such as:
-- _var_ variables are stored in window object while _const_ and _let_ are not.
-- script in html that has the _src_ attribute will not execute what is between its tags
-- forms with _GET_ request method will send _GET_ request along with queries in format <\input-nameAttribute>=<\input-value>
-- realized workaround: if i really for some reason need to send data along with GET request, it is possible via queries.
-- JS optional chaining operator (pretty new feature - 2020)
-- what term short-circuiting means
-- what is operator associativity and when it comes into picture
-- code practice
 
-### [demo](https://heno-only-members.herokuapp.com/)
+a lot of small things such as:
+
+-   _var_ variables are stored in window object while _const_ and _let_ are not.
+-   script in html that has the _src_ attribute will not execute what is between its tags
+-   forms with _GET_ request method will send _GET_ request along with queries in format <\input-nameAttribute>=<\input-value>
+-   realized workaround: if i really for some reason need to send data along with GET request, it is possible via queries.
+-   JS optional chaining operator (pretty new feature - 2020)
+-   what term short-circuiting means
+-   what is operator associativity and when it comes into picture
+-   code practice
+
+### [demo](https://only-members.onrender.com/)
 
 <hr>
 
-- _in case of any bugs please create an issue_
+-   _in case of any bugs please create an issue_
--- app.js
+++ app.js
@@ -17,34 +17,33 @@ const Users = require("./models/users");
 const AppStatistics = require("./models/appStatistics");
 const UserStatistics = require("./models/userStatistics");
 
-
 // environment variables
-const {MONGODB_URI, SECRET_KEY} = process.env;
+const { MONGODB_URI, SECRET_KEY } = process.env;
 
 // res global variables
 const adminsLimit = 10;
 const textColors = {
-  "default": "#ffffff",
-  "black": "#000000",
-  "member color": "#faff00",
-  "admin color": "#004e8d",
-  "light grey": "#c4c4c4c4",
-  "red": "#ff0000",
-  "green" : "#91c860",
-  "cyan": "#00ffff",
-}
+    default: "#ffffff",
+    black: "#000000",
+    "member color": "#faff00",
+    "admin color": "#004e8d",
+    "light grey": "#c4c4c4c4",
+    red: "#ff0000",
+    green: "#91c860",
+    cyan: "#00ffff",
+};
 // made separate object for shadow colors, because its default key
 // is none and default value is empty string
 const shadowColors = {
-  "none": "",
-  "black": "#000000",
-  "member color": "#faff00",
-  "admin color": "#004e8d",
-  "light grey": "#c4c4c4c4",
-  "red": "#ff0000",
-  "green" : "#91C860",
-  "cyan": "#00ffff",
-}
+    none: "",
+    black: "#000000",
+    "member color": "#faff00",
+    "admin color": "#004e8d",
+    "light grey": "#c4c4c4c4",
+    red: "#ff0000",
+    green: "#91C860",
+    cyan: "#00ffff",
+};
 
 // router variables
 const indexRouter = require("./routes/index");
@@ -54,24 +53,29 @@ const benefitsRouter = require("./routes/benefits");
 const boardsRouter = require("./routes/boards");
 
 // mongoose connection
-mongoose.connect(MONGODB_URI,{useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on(
+    "error",
+    console.error.bind(console, "MongoDB connection error:")
+);
 
-passport.serializeUser((user,done) =>{
-  done(null,user._id);
+passport.serializeUser((user, done) => {
+    done(null, user._id);
 });
 
-passport.deserializeUser((id,done) =>{
-  Users.findById(id)
-    .then(user =>{
-      done(null,user);
-    })
-    .catch(done);
+passport.deserializeUser((id, done) => {
+    Users.findById(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch(done);
 });
 /* initializing express app and working with just app till the end */
 const app = express();
-
 
 /* app configuration */
 
@@ -81,14 +85,18 @@ app.set("view engine", "ejs");
 // express-ejs-layout config
 app.set("layout", "layouts/main");
 
-
-
 // security code for production
 app.use(compression());
 app.use(helmet());
 
 app.use(logger("dev"));
-app.use(session({secret: SECRET_KEY, resave: false, saveUninitialized: true}));
+app.use(
+    session({
+        secret: SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
@@ -96,10 +104,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(expressEjsLayouts);
-app.use((req,res,next) =>{
-  res.header("Content-Security-Policy", "*");
-  next();
-})
+app.use((req, res, next) => {
+    res.header("Content-Security-Policy", "*");
+    next();
+});
 
 // custom app.js middleware functions
 app.use(setGlobalVariables);
@@ -113,61 +121,61 @@ app.use("/benefits", benefitsRouter);
 app.use("/boards", boardsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error =
+        req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error", {layout: false});
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error", { layout: false });
 });
-
 
 module.exports = app;
 
-
 // custom middleware functions
 
-function setGlobalVariables(req,res,next){
-  res.locals.adminsLimit = adminsLimit;
-  res.locals.currentUser = req.user;
-  res.locals.textColors = textColors;
-  res.locals.shadowColors = shadowColors;
-  next();
-}
-
-function statistics(req,res,next){
-  // excluding public stylesheets images and javascripts as they are automatically being asked for by the
-  // server and not by the user itself
-  if(req.url.startsWith("/images") || req.url.startsWith("/stylesheets") || req.url.startsWith("/javascripts")){
-    return next();
-  }
-  AppStatistics.findOne()
-  .then(statistics =>{
-      if(req.method == "GET")
-        statistics.visitedPages ++;
-      else if(req.method === "POST")
-        statistics.pageSubmissions ++;
-      else
-        return;
-      return statistics.save();
-    })
-    .catch(next)
-    if(req.user && req.method === "GET"){
-      UserStatistics.findById(req.user.statistics._id)
-        .then(userStatistics =>{
-          userStatistics.visitedPages ++;
-          return userStatistics.save();
-        })
-        .catch(next);
-    }
-    
+function setGlobalVariables(req, res, next) {
+    res.locals.adminsLimit = adminsLimit;
+    res.locals.currentUser = req.user;
+    res.locals.textColors = textColors;
+    res.locals.shadowColors = shadowColors;
     next();
 }
 
+function statistics(req, res, next) {
+    // excluding public stylesheets images and javascripts as they are automatically being asked for by the
+    // server and not by the user itself
+    if (
+        req.url.startsWith("/images") ||
+        req.url.startsWith("/stylesheets") ||
+        req.url.startsWith("/javascripts")
+    ) {
+        return next();
+    }
+    AppStatistics.findOne()
+        .then((statistics) => {
+            if (req.method == "GET") statistics.visitedPages++;
+            else if (req.method === "POST")
+                statistics.pageSubmissions++;
+            else return;
+            return statistics.save();
+        })
+        .catch(next);
+    if (req.user && req.method === "GET") {
+        UserStatistics.findById(req.user.statistics._id)
+            .then((userStatistics) => {
+                userStatistics.visitedPages++;
+                return userStatistics.save();
+            })
+            .catch(next);
+    }
+
+    next();
+}
